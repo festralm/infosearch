36caeaf2ead616a00976a692a4943475c502340b
Alia <alia01minn@gmail.com>
2023-03-03 00:33:53 +0300

    created tokenizer and implemented search in index

--- task2/main.py
+++ task2/main.py
@@ -1,44 +1,18 @@
-import re
-import spacy
-from spacy.symbols import ORTH
-from pathlib import Path
-from collections import defaultdict
-import io
 import codecs
+from tokenizer import Tokenizer
 
 if __name__ == '__main__':
-    # load model
-    nlp = spacy.load("ru_core_news_sm")
-    # nlp = spacy.load("en_core_web_trf")
 
-    # modify model
-    infixes = nlp.Defaults.infixes + [r'(<)']
-    nlp.tokenizer.infix_finditer = spacy.util.compile_infix_regex(infixes).finditer
-    nlp.tokenizer.add_special_case(f"<i>", [{ORTH: f"<i>"}])
-    nlp.tokenizer.add_special_case(f"</i>", [{ORTH: f"</i>"}])
+    tokenizer = Tokenizer()
 
-    directory = "../task1/pages/"
-    files = Path(directory).glob('*.txt')
+    tokens, lemmas = tokenizer.get_tokens_and_lemmas("../task1/pages/")
+
     out1 = codecs.open("tokens.txt", "w", "utf-8")
-    out2 = codecs.open("lemmas.txt", "w", "utf-8")
-    tokens = set()
-    lemmas = defaultdict(list)
-    it = 0
-    for filename in files:
-        it += 1
-        print(str(it) + ": " + str(filename))
-        with io.open(filename, encoding='utf-8') as file:
-            for line in file.readlines():
-                doc = nlp(line)
-                for token in doc:
-                    # check if token consists of letters only and has type == ADJ or ADV or NOUN or PRON or VERB
-                    if re.match(r"^[а-яА-Я]+$", token.text) and token.pos_ in {"ADJ", "ADV", "NOUN", "PRON", "VERB"} \
-                            and token.text not in tokens:  # check if token haven't been added already
-                        tokens.add(token.text)
-                        out1.write(token.text + "\n")
-                        lemmas[token.lemma_].append(token.text)
-
+    for token in tokens:
+        out1.write(token + "\n")
     out1.close()
+
+    out2 = codecs.open("lemmas.txt", "w", "utf-8")
     for k, v in lemmas.items():
         out2.write(k)
         for t in v:
--- task3/main.py
+++ task3/main.py
@@ -1,53 +1,11 @@
 import json
-import io
 import codecs
-import pathlib
-from pathlib import Path
-import spacy
-from spacy.symbols import ORTH
-from collections import defaultdict
-import re
-
-
-def get_inverted_index():
-    lemma_to_files = {}
-
-    nlp = spacy.load("ru_core_news_sm")
-
-    infixes = nlp.Defaults.infixes + [r'(<)']
-    nlp.tokenizer.infix_finditer = spacy.util.compile_infix_regex(infixes).finditer
-    nlp.tokenizer.add_special_case(f"<i>", [{ORTH: f"<i>"}])
-    nlp.tokenizer.add_special_case(f"</i>", [{ORTH: f"</i>"}])
-
-    directory = "../task1/pages/"
-    files = Path(directory).glob('*.txt')
-    it = 0
-    for filename in files:
-        file_name = str(filename)
-        it += 1
-        lemmas = set()
-        print(str(it) + ": " + file_name)
-        with io.open(filename, encoding='utf-8') as file:
-            for line in file.readlines():
-                doc = nlp(line)
-                for token in doc:
-                    # check if lemma consists of letters only
-                    # and has type == ADJ or ADV or NOUN or PRON or VERB
-                    lemma = token.lemma_
-                    if re.match(r"^[а-яА-Я]+$", token.text) \
-                            and token.pos_ in {"ADJ", "ADV", "NOUN", "PRON", "VERB"} \
-                            and lemma not in lemmas:  # check if lemma hasn't been added already
-                        lemmas.add(lemma)
-                        if lemma in lemma_to_files.keys():
-                            lemma_to_files[lemma].append(file_name)
-                        else:
-                            lemma_to_files[lemma] = [file_name]
-    return lemma_to_files
-
+from task2.tokenizer import Tokenizer
 
 if __name__ == '__main__':
-    result = get_inverted_index()
+    tokenizer = Tokenizer()
+    lemmas_to_files = tokenizer.get_lemmas_to_files("../task1/pages/")
 
-    json_string = json.dumps(result, ensure_ascii=False)
+    json_string = json.dumps(lemmas_to_files, ensure_ascii=False)
     with codecs.open("inverted_index.json", "w", "utf-8") as outfile:
         outfile.write(json_string)
